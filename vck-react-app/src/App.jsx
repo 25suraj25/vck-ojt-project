acb47a888ba8ff228b48b503807203c798096c91
Suraj Yadrave <surajyadrave71@gmail.com>
2025-06-27 16:07:07 +0530

    second commit

--- vck-react-app/src/App.jsx
+++ vck-react-app/src/App.jsx
@@ -23,7 +23,7 @@ const App = ()  =>{
           show={showPopup}
           onClose={handleClosePopup}
           studentName="Suraj Yadrave"
-          studentPhotoUrl="/images/saniya.jpg" // Path to their photo
+          studentPhotoUrl="/public/suraj.jpg" // Path to their photo
           uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
         />
       </div>
